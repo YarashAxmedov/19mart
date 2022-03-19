b9da96a3bdf9f9e7b8e346d20d4879bc5ea079a1
Firdavs <firdavssherzodovic@gmail.com>
2022-03-19 13:53:33 +0500

    jjjjjj

---
+++ out/data.js
@@ -0,0 +1,20 @@
+export let mass = [
+    {
+        names: 'samsung',
+        prise: 21,
+        desc: 'plyus',
+        img: "https://jimomaddinov.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fba2b44dc-4435-4fd8-a1a0-1c7436a43799%2FUntitled.png?table=block&id=606a2cf6-dc24-4d69-8b7a-13008fcd073b&spaceId=535b450e-a223-46e6-ae23-34490ba227f5&width=2000&userId=&cache=v2"
+    },
+    {
+        names: 'nokia',
+        prise: 52,
+        desc: 'yus',
+        img: "https://jimomaddinov.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fba2b44dc-4435-4fd8-a1a0-1c7436a43799%2FUntitled.png?table=block&id=606a2cf6-dc24-4d69-8b7a-13008fcd073b&spaceId=535b450e-a223-46e6-ae23-34490ba227f5&width=2000&userId=&cache=v2"
+    },
+    {
+        names: 'apple',
+        prise: 12,
+        desc: 'us',
+        img: "https://jimomaddinov.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fba2b44dc-4435-4fd8-a1a0-1c7436a43799%2FUntitled.png?table=block&id=606a2cf6-dc24-4d69-8b7a-13008fcd073b&spaceId=535b450e-a223-46e6-ae23-34490ba227f5&width=2000&userId=&cache=v2"
+    }
+];
---
+++ out/index.js
@@ -0,0 +1,32 @@
+import { mass } from './data.js';
+let container = document.querySelector('.container-sm');
+function rendr(b) {
+    let carrd = document.createElement('div');
+    carrd.className = "card";
+    carrd.style.width = "18rem";
+    let img = document.createElement('img');
+    carrd.appendChild(img);
+    img.src = b.img;
+    img.className = "card-img-top ratio ratio-1x1";
+    let card_body = document.createElement('div');
+    card_body.className = "card-body";
+    let card_title = document.createElement('h5');
+    card_title.className = "card-title";
+    card_title.innerText = b.names;
+    let card_text = document.createElement('p');
+    card_text.className = "card-text";
+    let text_success = document.createElement('h4');
+    text_success.className = "text-success";
+    text_success.innerText = '$' + b.prise;
+    let ahref = document.createElement('a');
+    ahref.className = "btn btn-primary w-50";
+    container.appendChild(carrd);
+    carrd.appendChild(card_body);
+    card_body.appendChild(card_title);
+    card_body.appendChild(card_text);
+    card_text.appendChild(text_success);
+    card_text.append(b.desc);
+    card_body.appendChild(ahref);
+}
+rendr(mass[0]);
+rendr(mass[1]);
